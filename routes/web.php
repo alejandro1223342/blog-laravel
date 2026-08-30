1ac417842f92bbd5da4c22d7cd34c72a527398bc
alejandro1223342 <alejomuoss@gmail.com>
2026-08-30 13:53:43 -0500

    feat/rutas en laravel

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/posts/create', function (){
+
+    return "Aqui se mostrara un formulario para crear un nuevo post";
+    
+});
+
+Route::get('/posts/{post}/{category?}', function ($post) {
+
+    return "Aqui se mostra el post {$post}";
+
+});
+
+
+// Route::get('/posts/{post}/{category}', function ($post,$category) {
+//     return "Aqui se mostra el post {$post} de la categoria {$category}";
+// });
